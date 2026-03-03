d7423e9d79b9e5e72a496d0e4fbcdad2d0f70ce2
Osmar238 <osmar77770@gmail.com>
2026-03-03 14:23:34 -0600

    First practice done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your name?");
+
+        Console.Write("My name is: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Wow!, and what is your last name?");
+
+        Console.Write("My last name is: ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Ok, so, you are {name} {last_name}");
+        Console.WriteLine("That´s right =)");
     }
 }
