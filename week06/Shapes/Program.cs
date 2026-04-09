e0f1b89e663c0f2977e6213b57ee2920418e4bcd
Osmar238 <osmar77770@gmail.com>
2026-04-09 17:44:38 -0600

    Homework

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> listaDeFiguras = new List<Shape>();
+
+        Square cuadrado = new Square("Rojo", 3);
+        Rectangle rectangulo = new Rectangle("Azul", 4, 5);
+        Circle circulo = new Circle("Verde", 6);
+
+        listaDeFiguras.Add(cuadrado);
+        listaDeFiguras.Add(rectangulo);
+        listaDeFiguras.Add(circulo);
+
+        foreach (Shape figura in listaDeFiguras)
+        {
+            string color = figura.GetColor();
+            double area = figura.GetArea();
+
+            Console.WriteLine($"La figura es de color {color} y su área es de {area}");
+        }
     }
 }
